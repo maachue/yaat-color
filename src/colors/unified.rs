b1da515adaec39af4edc37ccb0e5a1eaa0b31822
Maachue <109926858+maachue@users.noreply.github.com>
2025-12-29 20:50:28 +0700

    chore: add dialoguer

--- src/colors/unified.rs
+++ src/colors/unified.rs
@@ -30,6 +30,7 @@ impl<T> BasedAnsi<T> {
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         self.0.iter()
     }
+    #[cfg(feature = "seq_win")]
     pub fn get_ro(&self, index: usize) -> &T {
         return &self.0[index]
     }
